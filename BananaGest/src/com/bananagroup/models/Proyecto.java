9b273305f923e1cd4a79979bee9ea7a08c8fc036
A4Alumno <A4Alumno@PCAULA175.aules.local>
2017-05-31 19:48:33 +0200

    Añadidos Archivos de servletProyecto

--- BananaGest/src/com/bananagroup/models/Proyecto.java
+++ BananaGest/src/com/bananagroup/models/Proyecto.java
@@ -1,5 +1,35 @@
 package com.bananagroup.models;
 
+import java.util.Date;
+
 public class Proyecto {
+	// Definicion de los atributos de proyecto generico
+	private int idProyecto;
+	private String tituloPy;
+	private String descripcionPy;
+	private Date fechaInicioPy;
+	private String responsablePy;
+	private boolean activoPy;
+	// private Tarea[] tareasPy; //define un Array de las tareas del proyecto.
+	// Cada tarea se define con su claseModelo Tarea.java  PERO NO LA PRESENTAMOS EN ESTA LISTA
+
+	// public Proyecto(int idProyecto, String tituloPy, String descripcionPy,
+	// Date fechaInicioPy, String responsablePy, boolean activoPy, Tarea[]
+	// tareas) {
+
+
+	public Proyecto(int idProyecto, String tituloPy, String descripcionPy, int i, String responsablePy,
+			boolean activoPy) {
+		// Inicializacion de los atributos de Proyecto
+		this.idProyecto = idProyecto;
+		this.tituloPy = tituloPy;
+		this.descripcionPy = descripcionPy;
+		this.fechaInicioPy = fechaInicioPy;
+		this.responsablePy = responsablePy;
+		this.activoPy = activoPy;
+		//this.tareasPy = tareasPy;
+	}
+
+
 
 }
