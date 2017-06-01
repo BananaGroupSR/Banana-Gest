b48a222ab03a3161e78edf175b39e382993223f7
A4Alumno <A4Alumno@PCAULA175.aules.local>
2017-06-01 19:21:14 +0200

    Subida de Proyecto y servlet

--- BananaGest/src/com/bananagroup/controllers/ListaProyectosServlet.java
+++ BananaGest/src/com/bananagroup/controllers/ListaProyectosServlet.java
@@ -2,6 +2,7 @@ package com.bananagroup.controllers;
 
 import java.io.IOException;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.GregorianCalendar;
 
 import javax.servlet.ServletException;
@@ -23,7 +24,7 @@ public class ListaProyectosServlet extends HttpServlet {
 		HttpSession misession = (HttpSession) request.getSession();
 
 		if (misession.getAttribute("idUsuario") != null) {// "idUsuario"
-			Proyecto[] listaProyectos = {
+			Proyecto[] listaProyectosRicardo = {
 					new Proyecto(170101, "Proyecto 01 de Ricardo", "Descripción del Proyecto 01 de Ricardo",
 							new GregorianCalendar(2017, Calendar.JANUARY, 01).getTime(), "Ricardo", "Activo"),
 					new Proyecto(170720, "Proyecto 02 de Ricardo", "Descriccion del Proyecto 02 de Ricardo",
@@ -31,8 +32,9 @@ public class ListaProyectosServlet extends HttpServlet {
 					new Proyecto(170415, "Proyecto 03 de Ricardo", "Descriccion del Proyecto 03 de Ricardo",
 							new GregorianCalendar(2017, Calendar.APRIL, 15).getTime(), "Ricardo", "No Activo"),
 					new Proyecto(171030, "Proyecto 04 de Ricardo", "Descriccion del Proyecto 04 de Ricardo",
-							new GregorianCalendar(2017, Calendar.OCTOBER, 30).getTime(), "Ricardo", "No Activo"),
+							new GregorianCalendar(2017, Calendar.OCTOBER, 30).getTime(), "Ricardo", "No Activo") };
 
+			Proyecto[] listaProyectosJuana = {
 					new Proyecto(170201, "Proyecto 01 de Juana", "Descripción del Proyecto 01 de Juana",
 							new GregorianCalendar(2017, Calendar.FEBRUARY, 01).getTime(), "Juana", "Activo"),
 					new Proyecto(170820, "Proyecto 02 de Juana", "Descriccion del Proyecto 02 de Juana",
@@ -40,8 +42,9 @@ public class ListaProyectosServlet extends HttpServlet {
 					new Proyecto(170515, "Proyecto 03 de Juana", "Descriccion del Proyecto 03 de Juana",
 							new GregorianCalendar(2017, Calendar.MAY, 15).getTime(), "Juana", "No Activo"),
 					new Proyecto(171130, "Proyecto 04 de Juana", "Descriccion del Proyecto 04 de Juana",
-							new GregorianCalendar(2017, Calendar.NOVEMBER, 30).getTime(), "Juana", "No Activo"),
+							new GregorianCalendar(2017, Calendar.NOVEMBER, 30).getTime(), "Juana", "No Activo") };
 
+			Proyecto[] listaProyectosLuis = {
 					new Proyecto(170301, "Proyecto 01 de Luis", "Descripción del Proyecto 01 de Luis",
 							new GregorianCalendar(2017, Calendar.MARCH, 01).getTime(), "Luis", "Activo"),
 					new Proyecto(170920, "Proyecto 02 de Luis", "Descriccion del Proyecto 02 de Luis",
@@ -49,14 +52,39 @@ public class ListaProyectosServlet extends HttpServlet {
 					new Proyecto(170615, "Proyecto 03 de Luis", "Descriccion del Proyecto 03 de Luis",
 							new GregorianCalendar(2017, Calendar.JUNE, 15).getTime(), "Luis", "No Activo"),
 					new Proyecto(171230, "Proyecto 04 de Luis", "Descriccion del Proyecto 04 de Luis",
-							new GregorianCalendar(2017, Calendar.DECEMBER, 30).getTime(), "Luis", "No Activo"), };
+							new GregorianCalendar(2017, Calendar.DECEMBER, 30).getTime(), "Luis", "No Activo") };
 
-			request.setAttribute("listaProyectosAMostrar", listaProyectos);
-			request.getRequestDispatcher("listaproyectos.jsp").forward(request, response);
+			String email = request.getParameter("email");
 
-		} else {
-			misession.invalidate();
-			response.sendRedirect("login");
+			if (email.equals("ricardo@r.es")) {
+
+				misession.setAttribute("listaProyectosAMostrar", "ricardo@r.es");
+
+
+
+				request.setAttribute("listaProyectosAMostrar", listaProyectosRicardo);
+				request.getRequestDispatcher("listaproyectos.jsp").forward(request, response);
+			} else {
+				if (email.equals("juana@j.es")) {
+
+					misession.setAttribute("listaProyectosAMostrar", "juana@j.es");
+
+					request.setAttribute("listaProyectosAMostrar", listaProyectosJuana);
+					request.getRequestDispatcher("listaproyectos.jsp").forward(request, response);
+				} else {
+					if (email.equals("luis@l.es")) {
+
+						misession.setAttribute("listaProyectosAMostrar", "luis@l.es");
+
+						request.setAttribute("listaProyectosAMostrar", listaProyectosLuis);
+						request.getRequestDispatcher("listaproyectos.jsp").forward(request, response);
+					} else {
+						misession.invalidate();
+						response.sendRedirect("login");
+					}
+
+				}
+			}
 		}
 
 	}
--- BananaGest/src/com/bananagroup/models/Proyecto.java
+++ BananaGest/src/com/bananagroup/models/Proyecto.java
@@ -1,6 +1,6 @@
 package com.bananagroup.models;
 
-import java.time.LocalDate;
+
 import java.util.Date;
 
 public class Proyecto {
